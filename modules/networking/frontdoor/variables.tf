762f84b890bc8e05320185b281237ca53e95ec01
Ali Arslan <aliarslanmushtaqali@gmail.com>
2024-03-19 19:18:26 +0500

    adding module for frontdoor

---
+++ modules/networking/frontdoor/variables.tf
@@ -0,0 +1,23 @@
+variable "rgname" {
+  type    = string
+}
+
+variable "name" {
+  type    = string
+}
+
+variable "backend_name" {
+  type    = string
+}
+
+variable "backend_hostname" {
+  type    = string
+}
+
+variable "frontend_name" {
+  type    = string
+}
+
+variable "frontend_hostname" {
+  type    = string
+}
