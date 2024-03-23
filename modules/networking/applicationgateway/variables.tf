482fc0e10669d2a59f9147078daf5d0728350b4d
Ali Arslan <aliarslanmushtaqali@gmail.com>
2024-03-23 11:43:16 +0500

    adding module for application gateway

---
+++ modules/networking/applicationgateway/variables.tf
@@ -0,0 +1,27 @@
+variable "appgwname" {
+  type    = string
+  default = ""
+}
+variable "rgname" {
+  type    = string
+  default = ""
+}
+variable "location" {
+  type    = string
+  default = ""
+}
+variable "sku_name" {
+  type    = string
+  default = ""
+}
+variable "sku_tier" {
+  type    = string
+  default = ""
+}
+variable "sku_capacity" {
+  type    = string
+  default = ""
+}
+variable "subnet_id" {
+  default = ""
+}
