5306adee9368d04d5e44d29fd1d292758a5dbc5f
Ali Arslan <aliarslanmushtaqali@gmail.com>
2024-03-26 19:46:11 +0500

    configuring output variables for pip

--- modules/networking/publicip/variables.tf
+++ modules/networking/publicip/variables.tf
@@ -1,24 +1,19 @@
 variable "rgname" {
   type    = string
-  default = ""
 }
 
 variable "location" {
   type    = string
-  default = ""
 }
 
 variable "pip_name" {
   type    = string
-  default = ""
 }
 
 variable "sku" {
   type    = string
-  default = ""
 }
 
 variable "allocation_method" {
   type    = string
-  default = ""
 }
