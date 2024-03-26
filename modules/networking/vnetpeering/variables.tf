52880dc86c8a0d63b3a900321e68a080dc37e316
Ali Arslan <aliarslanmushtaqali@gmail.com>
2024-03-26 19:47:58 +0500

    removing default values

--- modules/networking/vnetpeering/variables.tf
+++ modules/networking/vnetpeering/variables.tf
@@ -1,21 +1,16 @@
 variable "rgname" {
   type    = string
-  default = ""
 }
 
 variable "vnet1_name" {
   type    = string
-  default = ""
 }
 variable "vnet1_id" {
   type    = string
-  default = ""
 }
 variable "vnet2_name" {
   type    = string
-  default = ""
 }
 variable "vnet2_id" {
   type    = string
-  default = ""
 }
